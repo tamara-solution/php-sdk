fea9dfa1f0ea3a5d9b4d4d0a4806b3d8bc0070ac
Tamara <tech@tamara.co>
2020-05-19 13:31:17 +0200

    Add checkout id in reponse

--- src/Tamara/Model/Checkout/CheckoutResponse.php
+++ src/Tamara/Model/Checkout/CheckoutResponse.php
@@ -8,42 +8,41 @@ class CheckoutResponse
 {
     public const
         ORDER_ID = 'order_id',
+        CHECKOUT_ID = 'checkout_id',
         CHECKOUT_URL = 'checkout_url';
 
     private $orderId;
-
     private $checkoutUrl;
+    private $checkoutId;
 
     public function __construct(array $response)
     {
         $this->orderId = $response[self::ORDER_ID];
         $this->checkoutUrl = $response[self::CHECKOUT_URL];
+        $this->checkoutId = $response[self::CHECKOUT_ID];
     }
 
-    /**
-     * @return string
-     */
     public function getOrderId(): string
     {
         return $this->orderId;
     }
 
-    /**
-     * @return string
-     */
     public function getCheckoutUrl(): string
     {
         return $this->checkoutUrl;
     }
 
-    /**
-     * @return array
-     */
+    public function getCheckoutId()
+    {
+        return $this->checkoutId;
+    }
+
     public function toArray(): array
     {
         return [
-            self::ORDER_ID => $this->getOrderId(),
-            self::CHECKOUT_URL => $this->getCheckoutUrl()
+            self::ORDER_ID     => $this->getOrderId(),
+            self::CHECKOUT_URL => $this->getCheckoutUrl(),
+            self::CHECKOUT_ID  => $this->getCheckoutId(),
         ];
     }
 }
